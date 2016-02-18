83aedb3187784555d435afa94928889c466b0bbe
cparker4 <charles.w.parker95@gmail.com>
2016-02-17 19:10:38 -0500

    knn.py done

--- p1/knn.py
+++ p1/knn.py
@@ -43,40 +43,46 @@ class KNN(BinaryClassifier):
         return    "w=" + repr(self.weights)
 
     def predict(self, X):
-        """
-        X is a vector that we're supposed to make a prediction about.
-        Our return value should be the 'vote' in favor of a positive
-        or negative label.  In particular, if, in our neighbor set,
-        there are 5 positive training examples and 2 negative
-        examples, we return 5-2=3.
+		"""
+		X is a vector that we're supposed to make a prediction about.
+		Our return value should be the 'vote' in favor of a positive
+		or negative label.  In particular, if, in our neighbor set,
+		there are 5 positive training examples and 2 negative
+		examples, we return 5-2=3.
 
-        Everything should be in terms of _Euclidean distance_, NOT
-        squared Euclidean distance or anything more exotic.
-        """
+		Everything should be in terms of _Euclidean distance_, NOT
+		squared Euclidean distance or anything more exotic.
+		"""
 
-        isKNN = self.opts['isKNN']     # true for KNN, false for epsilon balls
-        N     = self.trX.shape[0]      # number of training examples
+		isKNN = self.opts['isKNN']     # true for KNN, false for epsilon balls
+		N     = self.trX.shape[0]      # number of training examples
 
-        if self.trY.size == 0:
-            return 0                   # if we haven't trained yet, return 0
-        elif isKNN:
-            # this is a K nearest neighbor model
-            # hint: look at the 'argsort' function in numpy
-            K = self.opts['K']         # how many NN to use
+		if self.trY.size == 0:
+			return 0                   # if we haven't trained yet, return 0
+		elif isKNN:
+			# this is a K nearest neighbor model
+			# hint: look at the 'argsort' function in numpy
+			K = self.opts['K']         # how many NN to use
 
-            val = 0                    # this is our return value: #pos - #neg of the K nearest neighbors of X
-            ### TODO: YOUR CODE HERE
-            util.raiseNotDefined()
+			val = 0                    # this is our return value: #pos - #neg of the K nearest neighbors of X
 
-            return val
-        else:
-            # this is an epsilon ball model
-            eps = self.opts['eps']     # how big is our epsilon ball
+			norms = linalg.norm(self.trX-X, axis=1)
+			sortedIndices = argsort(norms)
 
-            val = 0                    # this is our return value: #pos - #neg within and epsilon ball of X
-            ### TODO: YOUR CODE HERE
-            util.raiseNotDefined()
-            return val
+			for i in range(0, K):
+				val += self.trY[sortedIndices[i]]
+
+			return val
+		else:
+			# this is an epsilon ball model
+			eps = self.opts['eps']     # how big is our epsilon ball
+
+			val = 0                    # this is our return value: #pos - #neg within and epsilon ball of X
+
+			norms = linalg.norm(self.trX-X, axis=1)
+			val = sum(self.trY[norms <= eps])          
+
+			return val
                 
             
 
