e4f3e71fbe3bfb4af2a5e01cbc201440dcc388fd
abajcsy <andrea.victoria.b@gmail.com>
2016-04-20 19:54:48 -0400

    touchups done

--- p2/gd_test.py
+++ p2/gd_test.py
@@ -2,12 +2,30 @@ import linear
 from imports import *
 import datasets
 import math
+import pylab
+import numpy
 
-x, trajectory = gd.gd(lambda x: x**3+x**10-x**2, lambda x: x*(10*x**8+3*x-2), -1, 100, 0.01)
-plot(trajectory)
-xlabel('Iteration Number')
-ylabel('Approximated Value')
+#x, trajectory = gd.gd(lambda x: x**3+x**10-x**2, lambda x: 3*(x**2)+10*(x**9)-2*x, -1, 100, 0.01)
+#plot(trajectory)
+#pylab.plot()
+#xlabel('Iteration Number')
+#ylabel('Approximated Value')
 #ylim([0,50])
-suptitle('Gradient Descent with Step Size = 0.5')
-show(True)
+#suptitle('Plot of x^3+x^10-x^2')
+#show(True)
 
+x = numpy.linspace(-15,15,1000) # 100 linearly spaced numbers
+y = x**3+x**10-x**2 # computing the values of sin(x)/x
+
+# compose plot
+pylab.plot(x,y) # x**3+x**10-x**2
+pylab.plot(0.606,-0.138, 'ro')
+pylab.plot(-0.911,-1.192, 'ro')
+#pylab.plot(x,y,'co') # same function with cyan dots
+#pylab.plot(x,2*y,x,3*y) # 2*sin(x)/x and 3*sin(x)/x
+xlabel('X-Value')
+ylabel('Y-Value')
+ylim([-3,3])
+xlim([-3,3])
+suptitle('Plot of x^3+x^10-x^2 and Local and Global Minima')
+pylab.show() # show the plot
